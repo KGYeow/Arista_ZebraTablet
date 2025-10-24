aa4f51c096509dd5d9d2960f5aa3fabc5e686f52
KGYeow <kokguan0819@gmail.com>
2025-10-24 12:32:34 +0800

    Database migration & update

---
+++ Arista_ZebraTablet/Arista_ZebraTablet.Shared/Data/Migrations/20251024030706_CreateIdentitySchemaV2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Arista_ZebraTablet.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIdentitySchemaV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Format",
+                table: "ScannedBarcode",
+                newName: "BarcodeType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BarcodeType",
+                table: "ScannedBarcode",
+                newName: "Format");
+        }
+    }
+}
